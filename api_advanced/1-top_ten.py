56548a0d7a2a89f7a05770b80c05ef5415c9624a
umumararu <umumararu@alustudent.com>
2025-07-14 01:04:34 -0700

    redit issues

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
-"""Print the titles of the first 10 Hot Posts for a subreddit"""
+"""Print the titles of the first 10 hot posts of a subreddit"""
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts from a subreddit"""
-    headers = {'User-Agent': 'MyAPI/0.0.1'}
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    """Fetch and print titles of top 10 hot posts"""
+    headers = {'User-Agent': 'AuroreRedditScript/1.0'}
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code != 200:
-            print(None)
-            return
-
-        data = response.json().get('data', {})
-        children = data.get('children', [])
-
-        for post in children[:10]:
-            print(post.get('data', {}).get('title'))
-    except Exception:
+    except requests.RequestException:
         print(None)
+        retur
+
