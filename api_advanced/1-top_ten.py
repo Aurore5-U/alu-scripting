8a0e14c91c4d95019c1db826c92d8ac0a11cef9e
umumararu <umumararu@alustudent.com>
2025-07-14 01:14:34 -0700

    redit

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,34 +5,41 @@ import requests
 
 def top_ten(subreddit):
     """Fetch and print titles of top 10 hot posts"""
-    if not subreddit or not isinstance(subreddit, str):
-        print(None)
+    if not subreddit:
+        print("None")
         return
-    
+
     headers = {'User-Agent': 'AuroreRedditScript/1.0'}
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    
+
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
     except requests.RequestException:
-        print(None)
+        print("None")
         return
-    
+
     if response.status_code != 200:
-        print(None)
+        print("None")
         return
-    
+
     try:
-        data = response.json().get("data", {}).get("children", [])
-    except (ValueError, KeyError):
-        print(None)
+        json_data = response.json()
+        data = json_data.get("data")
+        if not data:
+            print("None")
+            return
+        
+        children = data.get("children", [])
+        if not children:
+            print("None")
+            return
+            
+        for post in children:
+            post_data = post.get("data", {})
+            title = post_data.get("title")
+            if title:
+                print(title)
+                
+    except (ValueError, KeyError, TypeError):
+        print("None")
         return
-    
-    if not data:
-        print(None)
-        return
-    
-    for post in data:
-        title = post.get("data", {}).get("title")
-        if title:
-            print(title)
