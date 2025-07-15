51cbc26f9500a6ac9cf88609029ef2eb070898f9
umumararu <umumararu@alustudent.com>
2025-07-14 22:59:49 -0700

    api advanced

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,36 +1,37 @@
 #!/usr/bin/python3
-"""Print the titles of the first 10 hot posts of a subreddit"""
+"""
+1-top_ten: Print the titles of the first 10 hot posts of a given subreddit.
+"""
 import requests
 
 
 def top_ten(subreddit):
     """Fetch and print titles of top 10 hot posts"""
     headers = {'User-Agent': 'MyAPI/0.0.1'}
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
     except requests.RequestException:
         print("None")
         return
-    
+
     if response.status_code != 200:
         print("None")
         return
-    
+
     try:
         json_data = response.json()
-        data = json_data.get('data', {}).get('children', [])
-        
-        if not data:
+        posts = json_data.get('data', {}).get('children', [])
+
+        if not posts:
             print("None")
             return
-            
-        for post in data:
+
+        for post in posts:
             title = post.get('data', {}).get('title')
-            if title:
-                print(title)
-                
+            print(title)
+
     except (ValueError, KeyError, TypeError):
         print("None")
-        return
+
